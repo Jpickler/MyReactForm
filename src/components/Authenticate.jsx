d9670a9692c59cd8a97886b272f16abcb73846c1
jpickler <sirzymok@gmail.com>
2023-11-18 10:59:41 -0500

    complete. css styling and username checks done.

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -5,9 +5,10 @@ const Authenticate = ({token}) => {
 
   const [error, setError] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
+  const [authenticatedName, setAuthernticatedName] = useState(null);
 
 const handleClick =async() =>{
-    console.log(`auth button clicked`)
+    //console.log(`auth button clicked`)
     try{
       const sendData= await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate', 
               { 
@@ -19,10 +20,8 @@ const handleClick =async() =>{
               });
         const response=await sendData.json();
         setSuccessMessage(response.message);
+        setAuthernticatedName(response.data.username)
      
-
-
-
     }catch (error) {
       setError(error.message);
     }
@@ -33,9 +32,10 @@ const handleClick =async() =>{
 return (
   <>
     <h2>Authenticate</h2>
-    {error && <p>{error}</p>}
+    {error && <p class="warningMessage">{error}</p>}
     <button onClick={handleClick}>Press for Authentication</button>
-    {successMessage && <p> {successMessage}</p>}
+    {successMessage && <p class="successMessage"> {successMessage}</p>}
+    {authenticatedName && <p> Welcome {authenticatedName}</p>}
 
   </>
 )
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -6,10 +6,16 @@ const SignUpForm = ({setToken}) => {
   const [userName, setUserName] = useState(``);
   const [password, setPassword] = useState(``);
   const [error, setError] = useState(null);
+  const[displayWarning, setDisplayWarning] = useState(false);
+  const[formSubmitted,setFormSubmitted] =useState(false)
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(`form was submitted`);
+  
+
+    if (userName.length<8) setDisplayWarning(true); else{  // ensuring at least 8 characters in username start
+    
+    //console.log(`form was submitted`);
     try {
       const sendData = await fetch(`https://fsa-jwt-practice.herokuapp.com/signup`,
         {
@@ -24,22 +30,27 @@ const SignUpForm = ({setToken}) => {
         })
 
       const response = await sendData.json();
-      console.log(response);
+      //console.log(response);
+      setDisplayWarning(false);
+      setFormSubmitted(true);
       setToken(response.token);
     } catch (error) {
       setError(error.message)
     }
   }; // end handleSubmit
+};
 
   return (
     <>
       <h2> Sign Up Form</h2>
-      {error && <p>{error}</p>}
+      {error && <p class="warningMessage">{error}</p>}
+      {displayWarning && <p class="warningMessage">User Names must be a minimum of 8 characters</p>}
       <form onSubmit={handleSubmit}>
         <label>User Name: <input onChange={(event) => setUserName(event.target.value)} /> </label>
         <label>Password: <input onChange={(event) => setPassword(event.target.value)} /></label>
         <button>Submit</button>
       </form>
+      {formSubmitted && <p class="successMessage"> Your form has been successfully submitted!</p>}
 
     </>)
 } // end SignUpForm
